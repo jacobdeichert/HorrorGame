91b6da40c0f50e64becaca933d1c867334d10024
Odin von Doom <devinodin@gmail.com>
2014-02-23 14:57:42 -0500

    Fresh copy of expanded audio mechanic

---
+++ odin/AudioMechanicExpanded/Assets/Scripts/EnemyProperties.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyProperties : MonoBehaviour
+{
+	protected enum EnemyState { idle, wander, searchSlow, searchFast, chaseSlow, chaseFast, fight, shoot, dead }
+	protected EnemyState enemyState;
+	
+	protected int maxHealth;
+	protected int currentHealth;
+	protected bool canHitPlayer;
+	protected const float TURNSPEED = 4.0f;
+	protected float moveSpeed;
+	
+	public AudioClip sfxHurt;
+	public AudioClip sfxGrowl;
+	public AudioClip sfxAttack;
+	public AudioClip sfxDeath;
+
+	protected bool canGrowl = true;
+	
+	protected GameObject player;
+
+	void ApplyDamage(int damage)
+	{
+		currentHealth -= damage;
+		audio.PlayOneShot(sfxHurt);
+		if(currentHealth < 0)
+			currentHealth = 0;
+		// HUDandMenus.LastEnemyHitHealth(currentHealth, maxHealth);
+	}
+
+	/*
+	IEnumerator EnemyDeath()
+	{
+		animation.CrossFade("Fall");
+		audio.PlayOneShot(sfxDeath);
+		yield return new WaitForSeconds(animation.clip.length);
+		Destroy(gameObject);
+	}
+	*/
+
+	protected Quaternion FacePlayer(GameObject enemy, GameObject player, bool isRotatingPitch, float turnSpeed)
+	{
+		Vector3 playerDirection = player.transform.position - enemy.transform.position;
+		if(isRotatingPitch == false)
+			playerDirection.y = 0;
+		Quaternion rotation = Quaternion.LookRotation(playerDirection);
+		return Quaternion.Slerp(enemy.transform.rotation, rotation, Time.deltaTime * turnSpeed);
+	}
+}
