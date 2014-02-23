a035acbbf1069827288f1fcb6174d8671fd5d7bf
Odin von Doom <devinodin@gmail.com>
2014-02-23 14:48:03 -0500

    Beefed up enemy AI with Audio responsiveness
    There's still a lot of work to be done, but now the enemy is reacting
    generally how I'd like him to. Press 1,2,3, or 4 to make a different
    shape make a sound and make the enemy give chase.

---
+++ odin/AudioMechanicExpanded/Assets/Scripts/EnemyAIType1.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAIType1 : EnemyProperties
+{
+	// ANIMATION
+	float animationSpeed;
+	string animationName;
+
+	// SOUND DETECTION
+	Vector3 audioSourceDirection;
+
+	// Wander Redirection
+	RaycastHit redirectionHit;
+	bool turned;
+	float turnTimer = 0.0f;
+	Vector3[] rayDirection = new Vector3[50];
+	float theta;
+	const float REDIRECTIONRADIUS = 5.0f;
+	
+	// Chasing player
+	RaycastHit attackHit;
+	const float ATTACKRADIUS = 2.25f;
+	
+	//Attacking
+	bool isAttacking;
+	bool canDamagePlayer;
+	
+
+	void Start () 
+	{
+		maxHealth = 120;
+		currentHealth = 120;
+		enemyState = EnemyState.idle;
+		moveSpeed = 0.0f;
+		turnTimer = 2.0f;
+	}
+	
+	void Update ()
+	{
+		// Death
+		if(currentHealth <= 0)
+			enemyState = EnemyState.dead;
+		switch(enemyState)
+		{
+		case EnemyState.idle:
+			animation.Play("Idle");
+			break;
+		case EnemyState.wander:
+			if(turnTimer > 4.0f)
+			{
+				transform.Rotate(0.0f, Random.Range(135.0f, 225.0f), 0.0f);
+				turnTimer = 0.0f;
+			}
+			turnTimer += Time.deltaTime;
+			break;
+		case EnemyState.searchSlow:
+		case EnemyState.searchFast:
+		case EnemyState.chaseSlow:
+		case EnemyState.chaseFast:
+			transform.Translate(Vector3.forward * (moveSpeed*Time.deltaTime));
+			animation.CrossFade(animationName);
+			animation[animationName].speed = animationSpeed;
+			DetectAudioSourceToAttack();
+			break;
+		case EnemyState.fight:
+			animation.CrossFade("Slam");
+			DetectAudioSourceToAttack();
+			break;
+		case EnemyState.dead:
+			// StartCoroutine("EnemyDeath");
+			break;
+		}
+	}
+
+
+	void OnTriggerStay(Collider c)
+	{
+
+		if(c.gameObject.tag == "Player" && enemyState != EnemyState.fight)
+		{
+			enemyState = EnemyState.chaseFast;
+			player = c.gameObject;
+			if(canGrowl)
+			{
+				audio.PlayOneShot(sfxGrowl);
+				canGrowl = false;
+			}
+		}
+	}
+	void OnTriggerExit(Collider c)
+	{
+		if(c.gameObject.tag == "Player")
+		{
+			canGrowl = true;
+			enemyState = EnemyState.wander;
+		}
+	}
+	
+	private void DetectAudioSourceToAttack()
+	{
+		transform.rotation = FaceAudioSource(false);
+		
+		// ATTACK RAYCAST
+		isAttacking = false;
+		for(int i=0; i<rayDirection.Length; i++)
+		{
+			theta = (i+1)*(7.2f);
+			theta *= (Mathf.PI/180.0f);
+			rayDirection[i] = Vector3.Normalize(new Vector3(transform.localRotation.x + ATTACKRADIUS*Mathf.Sin(theta), 1.0f,
+				transform.localRotation.z + ATTACKRADIUS*Mathf.Cos(theta)));
+			Debug.DrawRay(transform.position, rayDirection[i] * ATTACKRADIUS, Color.red);
+			
+			if(Physics.Raycast(transform.position, rayDirection[i], out attackHit, ATTACKRADIUS))
+			{
+				if(attackHit.transform.gameObject.tag == "Player")
+				{
+					enemyState = EnemyState.fight;
+					isAttacking = true;
+				}
+			}
+		}
+		if(isAttacking == false)
+			enemyState = EnemyState.chaseSlow;
+	}
+
+	private Quaternion FaceAudioSource(bool isRotatingPitch)
+	{
+		if(isRotatingPitch == false)
+			audioSourceDirection.y = 0;
+		Quaternion rotation = Quaternion.LookRotation(audioSourceDirection);
+		return Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * TURNSPEED);
+	}
+
+	void OnHearSound(SoundSourceInfo sourceInfo)
+	{
+		// DETECT SOUND
+		audioSourceDirection = sourceInfo.transform.position - transform.position;
+		float audioSourceDistance = audioSourceDirection.magnitude;
+
+		// Volume is increased 100 fold for ease of use with distance.
+		// Distance is subtracted to resemble the monster's perception of the source's volume, making volume decay linear, which might be inaccurate.
+		float audioSourceRelativeVolume = (sourceInfo.volume * 100) - audioSourceDistance;
+		audioSourceDirection.Normalize();
+
+		// DETERMINE RESPONSE LEVEL
+		if(audioSourceRelativeVolume > 0 && audioSourceRelativeVolume < 20)
+		{
+			enemyState = EnemyState.wander;
+			moveSpeed = 2.0f;
+			animationSpeed = 0.5f;
+			animationName = "Walk";
+		}
+		else if(audioSourceRelativeVolume > 19 && audioSourceRelativeVolume < 40)
+		{
+			enemyState = EnemyState.searchSlow;
+			moveSpeed = 3.0f;
+			animationSpeed = 0.7f;
+			animationName = "Walk";
+		}
+		else if(audioSourceRelativeVolume > 39 && audioSourceRelativeVolume < 60)
+		{
+			enemyState = EnemyState.searchFast;
+			moveSpeed = 4.0f;
+			animationSpeed = 0.9f;
+			animationName = "Walk";
+		}
+		else if(audioSourceRelativeVolume > 59 && audioSourceRelativeVolume < 80)
+		{
+			enemyState = EnemyState.chaseSlow;
+			moveSpeed = 6.0f;
+			animationSpeed = 0.7f;
+			animationName = "Run";
+		}
+		else if(audioSourceRelativeVolume > 79)
+		{
+			enemyState = EnemyState.chaseFast;
+			moveSpeed = 7.5f;
+			animationSpeed = 0.9f;
+			animationName = "Run";
+		}
+	}
+}
---
+++ odin/AudioMechanicExpanded/Assets/Scripts/MessageBoard.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MessageBoard : MonoBehaviour
+{
+	Rect msgOutRect = new Rect (Screen.width/2 - 200, Screen.height - 450, 400, 375);
+	bool boardActive = false;
+
+	MessagePost newMsg;
+	Queue<MessagePost> msgQueue = new Queue<MessagePost>();
+	string msgTotal;
+
+	AudioPresets makeSound;
+
+	void Start()
+	{
+		makeSound = gameObject.GetComponent<AudioPresets>();
+	}
+	
+	void Update ()
+	{
+		if (msgQueue.Count > 0)
+		{
+			boardActive = true;
+			msgTotal = "";
+			MessagePost tmp = msgQueue.Peek();
+
+			if (tmp.RemovalTimer < 0)
+				msgQueue.Dequeue();
+
+			foreach(MessagePost post in msgQueue)
+			{
+				msgTotal += post.Message + "\n";
+
+				post.RemovalTimer -= Time.deltaTime;
+			}
+		}
+		else
+		{
+			boardActive = false;
+		}
+	}
+
+	public void Post(string message)
+	{
+		newMsg = new MessagePost (message);
+		msgQueue.Enqueue (newMsg);
+
+		AudioClip sound;
+		if (makeSound.presets.TryGetValue (message, out sound))
+			audio.PlayOneShot (sound);
+	}
+
+	void OnGUI()
+	{
+		if(boardActive)
+		{
+			GUI.TextArea(msgOutRect, msgTotal);
+		}
+	}
+}
