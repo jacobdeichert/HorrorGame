036141f82500f2c6f60ad365b121cf7023e865f0
HatOfChaos <roody-cuzzy14@hotmail.com>
2014-03-11 14:51:23 -0400

    More than one path.Custom Player Added w/ flashlight

--- Eric/HorrorGame/Assets/Scripts/MazeGenerator.cs
+++ Eric/HorrorGame/Assets/Scripts/MazeGenerator.cs
@@ -11,7 +11,10 @@ public class MazeGenerator : MonoBehaviour {
     public GameObject wall;
     public GameObject end;
     public GameObject floor;
-    public GameObject player1;
+   // public GameObject player1;
+    public GameObject player2;
+    public GameObject trap;
+    private int trapSpawnChance = 10;
     private static System.Random rand = new System.Random();
 
 	// Use this for initialization
@@ -19,20 +22,26 @@ public class MazeGenerator : MonoBehaviour {
         maze = GenerateMaze(mapheight,mapwidth);
         wall.transform.localScale = new Vector3(1*wallSize,1*wallSize,1*wallSize);
         floor.transform.localScale = new Vector3(1 * wallSize, 1 * wallSize, 1 * wallSize);
+        trap.transform.localScale = new Vector3(1 * wallSize, 1 * wallSize, 1 * wallSize);
         end.transform.localScale = new Vector3(1 * wallSize, 1 * wallSize, 1 * wallSize);
         Vector3 Player1pos = new Vector3(1*wallSize,0,1*wallSize);
-        GameObject Player1 = Instantiate(player1) as GameObject;
-        Player1.transform.position = Player1pos;
+        Vector3 Player2pos = new Vector3(1 * wallSize, 0, (mapwidth-2) * wallSize);
+        //GameObject Player1 = Instantiate(player1) as GameObject;
+        GameObject Player2 = Instantiate(player2) as GameObject;
+       // Player1.transform.position = Player1pos;
+        Player2.transform.position = Player2pos;
         
         for (int i = 0; i < mapheight; i++)
         {
             for(int j = 0;j<mapwidth;j++)
             {
-                
+
+                Vector3 Floorpos = new Vector3(i * wallSize, 0 - wallSize, j * wallSize);
+                GameObject Floor = Instantiate(floor) as GameObject;
                 if (!(maze[i, j] == 1))
                 {
-                    Vector3 Floorpos = new Vector3(i * wallSize, 0 - wallSize, j * wallSize);
-                    GameObject Floor = Instantiate(floor) as GameObject;
+                    //Vector3 Floorpos = new Vector3(i * wallSize, 0 - wallSize, j * wallSize);
+                    
                     if (Floor != null)
                     {
                         Floor.transform.position = Floorpos;
@@ -43,18 +52,65 @@ public class MazeGenerator : MonoBehaviour {
                 {
                     Vector3 pos = new Vector3(i * wallSize, 0, j * wallSize);
                     GameObject Wall = Instantiate(wall) as GameObject;
-
+                    GameObject Trap = Instantiate(trap) as GameObject;
+                    
                     if (Wall != null)
                     {
-                      
-                            Wall.transform.position = pos;
-                        
-                    }
+                        switch (Random.Range(0,15))
+                        {
+                            case 1:
+                               // if ((pos.x - Player1pos.x >= wallSize * 2) && (pos.z - Player1pos.z >= wallSize * 2))
+                                //{
 
+                                    Floor.transform.position = Floorpos;
+                                    //Trap.transform.position = pos;
+                                //}
+                                break;
+
+                            default:
+                                if (i == mapheight - 2 && j == mapwidth - 1)
+                                {
+                                    Floor.transform.position = Floorpos;
+                                }
+                                else
+                                {
+                                    Wall.transform.position = pos;
+                                }
+                               
+                                break;
+                        }
+                        switch (Random.Range(0, 15))
+                        {
+                            case 1:
+                                if ((pos.x - Player1pos.x >= wallSize * 2) && (pos.z - Player1pos.z >= wallSize * 2))
+                                {
+                                Trap.transform.position = pos;
+                                }
+                                break;
+
+                            default:
+                                break;
+                        }
+                        if (i == 0||i==mapheight-1||j==0||j==mapwidth-1)
+                        {
+                            if (i == mapheight - 2 && j == mapwidth - 1)
+                            {
+                            }
+                            else
+                            {
+                                Wall.transform.position = pos;
+                            }
+                        }
+                        else
+                        {
+                           // Wall.transform.position = pos;    
+                        }
+                        
+                             
+                    }
                 }
             }
         }
-
 	}
 
     private int[,] GenerateMaze(int height, int width)
---
+++ Eric/HorrorGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (BoxCollider))]
+
+public class PlayerController : MonoBehaviour {
+	
+	public float walkSpeed;
+	public float runSpeed;
+	public float strafeSpeed;
+	public float gravity;
+	public float jumpHeight;
+	public bool canJump;
+	bool isRunning = false;
+	bool isGrounded = false;
+
+	void Awake () {
+		rigidbody.freezeRotation = true;
+		rigidbody.useGravity = false;
+	}
+	
+	void FixedUpdate () {
+		// get correct speed
+		float forwardAndBackSpeed = walkSpeed;
+
+		if (isRunning) {
+			forwardAndBackSpeed = runSpeed;
+		}
+
+		// calculate how fast we should be moving
+		Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
+		targetVelocity = transform.TransformDirection(targetVelocity);
+		
+		// apply a force that attempts to reach our target velocity
+		Vector3 velocity = rigidbody.velocity;
+		Vector3 velocityChange = (targetVelocity - velocity);
+		velocityChange.y = 0;
+		rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+		
+		// jump
+		if (canJump && isGrounded && Input.GetButton("Jump")) {
+			rigidbody.velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z);
+			isGrounded = false;
+		}
+		
+		// apply gravity
+		rigidbody.AddForce(new Vector3 (0, -gravity * rigidbody.mass, 0));
+		//isGrounded = false;
+	}
+
+	void Update() {
+		if (isGrounded && Input.GetKeyDown(KeyCode.LeftControl)) {
+			isRunning = true;
+		}
+		
+		if (Input.GetKeyUp(KeyCode.LeftControl)) {
+			isRunning = false;
+		}
+	}
+	
+	void OnCollisionStay () {
+		isGrounded = true;
+	}
+
+
+}
