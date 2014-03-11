3fd8ea4a694f71945336629e57e77b1563508867
Matt Joncas <mattjoncas@users.noreply.github.com>
2014-03-11 14:56:40 -0400

    added basic pathfinding project.
    - pathfinding class returns a list of nodes
    - pathfinding class takes in a start & end node at the moment but that
    can be changed if need be
    - lags while generating a path, still trying to speed it up
    
    - ignore the binary heap class, it doesn't work properly

---
+++ matt/Pathfinding/Assets/Test.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Test : MonoBehaviour {
+	
+	private List<Node> nodes = new List<Node>();
+	private List<Node> path = new List<Node>();
+	private Node start;
+	private Node end;
+
+	//sets the end target for now
+	//sets up all nodes in the scene in a list
+	void Start () {
+		gameObject.AddComponent<PathGen>();
+		end = GameObject.Find ("End").GetComponent<Node>();
+		foreach (Node node in GameObject.FindObjectsOfType<Node>()){
+			nodes.Add(node);
+		}
+	}
+	
+	//resets nodes
+	//calculates path then sets nodes path true to show path
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E)){
+			ResetPath();
+			path = gameObject.GetComponent<PathGen>().CalculatePath(start, end, nodes);
+			if (path.Count > 0){
+				foreach (Node node in path){
+					if (node != null){
+						//sets nodes path true to change it's color
+						node.path = true;
+					}
+				}
+			}
+			else{
+				Debug.Log("No path.");
+			}
+		}
+	}
+	//resets all the nodes parents
+	//and sets a new start node based on players position
+	private void ResetPath(){
+		Vector2 player = new Vector2(Mathf.Round(GameObject.Find("3rd Person Controller").transform.position.x), Mathf.Round(GameObject.Find("3rd Person Controller").transform.position.z));
+		foreach (Node node in nodes){
+			node.path = false;
+			node.parent = null;
+			if (new Vector2(node.transform.position.x, node.transform.position.z) == player){
+				start = node;
+			}
+		}
+	}
+}
