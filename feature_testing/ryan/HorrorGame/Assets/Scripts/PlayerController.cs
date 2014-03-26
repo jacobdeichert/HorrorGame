c2854f20856c242532bd7be059eafb438a59380b
Ryan Allan James Holubeshen <Ryan.aj.holubeshen@bell.net>
2014-03-26 15:03:45 -0400

    skeleton model in for player

--- feature_testing/ryan/HorrorGame/Assets/Scripts/PlayerController.cs
+++ feature_testing/ryan/HorrorGame/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,12 @@ using System.Collections;
 [RequireComponent (typeof (BoxCollider))]
 
 public class PlayerController : MonoBehaviour {
-	
+	public string[] actions;
+	public int indexAction;						// what action should be played
+
+	public GUIText statusGUI;					// where the action is displayed
+
+	public float animationTime = 0;				// how long animations take
 	public float walkSpeed;
 	public float runSpeed;
 	public float strafeSpeed;
@@ -18,20 +23,26 @@ public class PlayerController : MonoBehaviour {
 	void Awake () {
 		rigidbody.freezeRotation = true;
 		rigidbody.useGravity = false;
+		indexAction = 0;
+		animation.wrapMode = WrapMode.Once;
+		animation["run"].wrapMode = WrapMode.Loop;
 	}
 	
 	void FixedUpdate () {
 		// get correct speed
 		float forwardAndBackSpeed = walkSpeed;
-
+		   
 		if (isRunning) {
 			forwardAndBackSpeed = runSpeed;
+			animation["run"].speed = 1.0f;
+			animation.CrossFade("run");
 		}
 
 		// calculate how fast we should be moving
 		Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
 		targetVelocity = transform.TransformDirection(targetVelocity);
-		
+		animation["run"].speed = 0.5f;
+		animation.CrossFade("run");
 		// apply a force that attempts to reach our target velocity
 		Vector3 velocity = rigidbody.velocity;
 		Vector3 velocityChange = (targetVelocity - velocity);
@@ -52,16 +63,22 @@ public class PlayerController : MonoBehaviour {
 	void Update() {
 		if (isGrounded && Input.GetKeyDown(KeyCode.LeftControl)) {
 			isRunning = true;
+			
 		}
 		
 		if (Input.GetKeyUp(KeyCode.LeftControl)) {
 			isRunning = false;
+			animation["run"].speed = 0.5f;
+			animation.CrossFade("run");	
 		}
 	}
 	
 	void OnCollisionStay () {
 		isGrounded = true;
 	}
-
+	void OnIdle() 
+	{
+		
+	}
 
 }
