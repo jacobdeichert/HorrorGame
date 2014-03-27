8cdc058b3c426228b9b9a39f6ffefdf7db585dce
Ryan Allan James Holubeshen <Ryan.aj.holubeshen@bell.net>
2014-03-26 20:21:42 -0400

    animation for player works . need to figure out logic now.
    Todo.... issue with character controller , need to figure out logic when
    standing still , to walking / running.

--- feature_testing/ryan/HorrorGame/Assets/Scripts/PlayerController.cs
+++ feature_testing/ryan/HorrorGame/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent (typeof (Rigidbody))]
-[RequireComponent (typeof (BoxCollider))]
+
 
 public class PlayerController : MonoBehaviour {
-	public string[] actions;
-	public int indexAction;						// what action should be played
-
+	public string actions = "idle";
+	//public int indexAction;						// what action should be played
+	
 	public GUIText statusGUI;					// where the action is displayed
-
-	public float animationTime = 0;				// how long animations take
+	private Vector3 idlepoint;
+	public float animationTime = 1.0f;				// how long animations take
 	public float walkSpeed;
 	public float runSpeed;
 	public float strafeSpeed;
@@ -19,62 +20,77 @@ public class PlayerController : MonoBehaviour {
 	public bool canJump;
 	bool isRunning = false;
 	bool isGrounded = false;
-
+	bool isIdle = false;
 	void Awake () {
 		rigidbody.freezeRotation = true;
 		rigidbody.useGravity = false;
-		indexAction = 0;
-		animation.wrapMode = WrapMode.Once;
-		animation["run"].wrapMode = WrapMode.Loop;
+
+		animation.Play("idle");
 	}
 	
 	void FixedUpdate () {
 		// get correct speed
 		float forwardAndBackSpeed = walkSpeed;
-		   
-		if (isRunning) {
+		//idlepoint = new Vector3(0,0,0);
+		
+		if (isRunning)
+		{
 			forwardAndBackSpeed = runSpeed;
-			animation["run"].speed = 1.0f;
+			animation["run"].speed = animationTime ;
 			animation.CrossFade("run");
 		}
+		else if (!isRunning)
+		{
+			animation["run"].speed = 0.5f;
+			animation.CrossFade("run");
+		}
+	   
 
 		// calculate how fast we should be moving
-		Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
-		targetVelocity = transform.TransformDirection(targetVelocity);
-		animation["run"].speed = 0.5f;
-		animation.CrossFade("run");
-		// apply a force that attempts to reach our target velocity
-		Vector3 velocity = rigidbody.velocity;
-		Vector3 velocityChange = (targetVelocity - velocity);
-		velocityChange.y = 0;
-		rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-		
-		// jump
-		if (canJump && isGrounded && Input.GetButton("Jump")) {
-			rigidbody.velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z);
-			isGrounded = false;
+		if (!isIdle)
+		{
+			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
+			targetVelocity = transform.TransformDirection(targetVelocity);
+			animation["run"].speed = animationTime / 2.0f;
+			animation.Play("run");
+			// apply a force that attempts to reach our target velocity
+			Vector3 velocity = rigidbody.velocity;
+			Vector3 velocityChange = (targetVelocity - velocity);
+			velocityChange.y = 0;
+			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+			if (canJump && isGrounded && Input.GetButton("Jump"))
+			{
+				rigidbody.velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z);
+				isGrounded = false;
+			}
 		}
+		 else if (isIdle) { animation.Play("idle"); }
+		// jump
+		
 		
 		// apply gravity
 		rigidbody.AddForce(new Vector3 (0, -gravity * rigidbody.mass, 0));
 		//isGrounded = false;
 	}
 
-	void Update() {
-		if (isGrounded && Input.GetKeyDown(KeyCode.LeftControl)) {
+	void Update()
+	{
+		if (isGrounded && Input.GetKeyDown(KeyCode.LeftControl))
+		{
 			isRunning = true;
-			
+
 		}
-		
-		if (Input.GetKeyUp(KeyCode.LeftControl)) {
+
+		if (Input.GetKeyUp(KeyCode.LeftControl))
+		{
 			isRunning = false;
-			animation["run"].speed = 0.5f;
-			animation.CrossFade("run");	
+
 		}
 	}
 	
 	void OnCollisionStay () {
 		isGrounded = true;
+		//animation.Play("idle");
 	}
 	void OnIdle() 
 	{
