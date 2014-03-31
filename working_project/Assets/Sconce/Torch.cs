36a7cda78999d03d5d28dbc73cec45a4a68ae5f3
Matt Joncas <mattjoncas@users.noreply.github.com>
2014-03-31 01:54:44 -0400

    added Torch/Sconce prefab
    - Added some materials and the model.
    - Added a torch script that toggles the particle system and light
    on/off.
    - Particle system is pretty basic right now, might try and add more to
    it.
    - Need to spawn torches throughout the maze still.
    - Need to add player interaction to the players controls using raycasts
    probably.

---
+++ working_project/Assets/Sconce/Torch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+
+	private bool flameOn = true;
+
+	// Use this for initialization
+	void Start () {
+		ToggleFlame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F)){
+			ToggleFlame();
+		}
+	}
+
+	private void ToggleFlame(){
+		if (flameOn){
+			flameOn = false;
+			transform.Find("Fire").particleSystem.enableEmission = false;
+			transform.Find("Fire Light").light.enabled = false;
+		}
+		else{
+			flameOn = true;
+			transform.Find("Fire").particleSystem.enableEmission = true;
+			transform.Find("Fire Light").light.enabled = true;
+		}
+	}
+}
