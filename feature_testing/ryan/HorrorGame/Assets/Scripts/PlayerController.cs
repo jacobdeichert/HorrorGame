875a7665eb591088981face1fb99b03603162a4c
Ryan Allan James Holubeshen <Ryan.aj.holubeshen@bell.net>
2014-03-31 01:21:27 -0400

    Animations done.
    Animations are done on the Player Character. ill clean up this project
    soon.

--- feature_testing/ryan/HorrorGame/Assets/Scripts/PlayerController.cs
+++ feature_testing/ryan/HorrorGame/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 [RequireComponent (typeof (Rigidbody))]
-
+[RequireComponent (typeof (BoxCollider))]
 
 public class PlayerController : MonoBehaviour {
-	public string actions = "idle";
-	//public int indexAction;						// what action should be played
 	
-	public GUIText statusGUI;					// where the action is displayed
-	private Vector3 idlepoint;
-	public float animationTime = 1.0f;				// how long animations take
 	public float walkSpeed;
 	public float runSpeed;
 	public float strafeSpeed;
@@ -20,81 +14,65 @@ public class PlayerController : MonoBehaviour {
 	public bool canJump;
 	bool isRunning = false;
 	bool isGrounded = false;
-	bool isIdle = false;
+
 	void Awake () {
 		rigidbody.freezeRotation = true;
 		rigidbody.useGravity = false;
-
-		animation.Play("idle");
 	}
 	
 	void FixedUpdate () {
 		// get correct speed
 		float forwardAndBackSpeed = walkSpeed;
-		//idlepoint = new Vector3(0,0,0);
-		
-		if (isRunning)
-		{
-			forwardAndBackSpeed = runSpeed;
-			animation["run"].speed = animationTime ;
-			animation.CrossFade("run");
-		}
-		else if (!isRunning)
-		{
-			animation["run"].speed = 0.5f;
-			animation.CrossFade("run");
-		}
-	   
 
-		// calculate how fast we should be moving
-		if (!isIdle)
-		{
-			Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
-			targetVelocity = transform.TransformDirection(targetVelocity);
-			animation["run"].speed = animationTime / 2.0f;
-			animation.Play("run");
-			// apply a force that attempts to reach our target velocity
-			Vector3 velocity = rigidbody.velocity;
-			Vector3 velocityChange = (targetVelocity - velocity);
-			velocityChange.y = 0;
-			rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-			if (canJump && isGrounded && Input.GetButton("Jump"))
-			{
-				rigidbody.velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z);
-				isGrounded = false;
-			}
+        // if running, set run speed
+		if (isRunning) {
+			forwardAndBackSpeed = runSpeed;
 		}
-		 else if (isIdle) { animation.Play("idle"); }
-		// jump
+
+		// calculate how fast it should be moving
+		Vector3 targetVelocity = new Vector3(Input.GetAxis("Horizontal") * strafeSpeed, 0, Input.GetAxis("Vertical") * forwardAndBackSpeed);
+		targetVelocity = transform.TransformDirection(targetVelocity);
 		
+		// apply a force that attempts to reach our target velocity
+		Vector3 velocity = rigidbody.velocity;
+		Vector3 velocityChange = (targetVelocity - velocity);
+		velocityChange.y = 0;
+		rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+		
+		// jump
+		if (canJump && isGrounded && Input.GetButton("Jump")) {
+			rigidbody.velocity = new Vector3(velocity.x, Mathf.Sqrt(2 * jumpHeight * gravity), velocity.z);
+			isGrounded = false;
+		}
 		
 		// apply gravity
 		rigidbody.AddForce(new Vector3 (0, -gravity * rigidbody.mass, 0));
-		//isGrounded = false;
 	}
 
-	void Update()
-	{
-		if (isGrounded && Input.GetKeyDown(KeyCode.LeftControl))
-		{
+	void Update() {
+        // check if the player is touching a surface below them
+        checkGrounded();
+
+        // check if the player is running
+		if (isGrounded && Input.GetKeyDown(KeyCode.LeftShift)) {
 			isRunning = true;
-
 		}
-
-		if (Input.GetKeyUp(KeyCode.LeftControl))
-		{
+		
+        // check if the player stops running
+		if (Input.GetKeyUp(KeyCode.LeftShift)) {
 			isRunning = false;
-
 		}
 	}
 	
-	void OnCollisionStay () {
-		isGrounded = true;
-		//animation.Play("idle");
+	void checkGrounded() {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.up * -1);
+
+        // if there is something directly below the player
+        if (Physics.Raycast(ray, out hit, collider.bounds.size.y / 2)) {
+            isGrounded = true;
+        }
 	}
-	void OnIdle() 
-	{
-		
-	}
+
 
 }
