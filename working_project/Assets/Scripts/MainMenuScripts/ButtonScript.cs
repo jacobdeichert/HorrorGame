0aaf9187e97880efdf842e9d4b4857c7bb7348ce
VagabondOfHell <arrow_m13@hotmail.com>
2014-04-01 13:09:24 -0400

    Main Menu and Networking
    -Main Menu thrown together
    -Network gets initialised...can't test until near wired internet or at
    home
    -Can't do network data until I can touch prefabs and scenes

---
+++ working_project/Assets/Scripts/MainMenuScripts/ButtonScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+    public Texture HoveredTexture;
+    public Texture DefaultTexture;
+    public Texture DisabledTexture;
+
+    bool enabled = true;
+
+	// Use this for initialization
+	void Start () {
+        guiTexture.texture = DefaultTexture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void Disable()
+    {
+        enabled = false;
+        guiTexture.texture = DisabledTexture;
+    }
+
+    public void Enable()
+    {
+        enabled = true;
+        guiTexture.texture = DefaultTexture;
+    }
+
+    void OnMouseOver()
+    {
+        if(enabled)
+            guiTexture.texture = HoveredTexture;
+    }
+
+    void OnMouseExit()
+    {
+        if (enabled)
+            guiTexture.texture = DefaultTexture;
+        else
+            guiTexture.texture = DisabledTexture;
+    }
+}
---
+++ working_project/Assets/Scripts/MainMenuScripts/Globals.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Globals : MonoBehaviour {
+
+
+    // Make this game object and all its transform children
+    // survive when loading a new scene.
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
