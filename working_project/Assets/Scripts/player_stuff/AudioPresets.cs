ba926dbdb3aed69c7b0684f1e0cfab5ef9b3ffc6
Odin von Doom <devinodin@gmail.com>
2014-04-13 23:16:52 -0400

    making audio adjustments

--- working_project/Assets/Scripts/player_stuff/AudioPresets.cs
+++ working_project/Assets/Scripts/player_stuff/AudioPresets.cs
@@ -6,14 +6,16 @@ public class AudioPresets : MonoBehaviour
 {
 	public SortedDictionary<string, AudioClip> presets = new SortedDictionary<string, AudioClip>();
 
-	public AudioClip jump;
-	public AudioClip select;
-	public AudioClip woosh;
+	public AudioClip help;
+	public AudioClip overHere;
+	public AudioClip runAway;
+    public AudioClip watchOut;
 
 	void Start ()
 	{
-		presets.Add ("jump", jump);
-		presets.Add ("select", select);
-		presets.Add ("woosh", woosh);
+        presets.Add("help", help);
+        presets.Add("over here", overHere);
+        presets.Add("run away", runAway);
+        presets.Add("watch out", watchOut);
 	}
 }
