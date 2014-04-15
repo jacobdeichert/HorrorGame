d08451d16a19c9d16329d6680f97454d7d396b52
HatOfChaos <roody-cuzzy14@hotmail.com>
2014-04-15 17:29:21 -0400

    Main Menu

---
+++ working_project/Assets/Scripts/MainMenuScripts/FlickeringLight.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickeringLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	
+	}
+    void FixedUpdate()
+    {
+        gameObject.light.intensity = Random.Range(1.0f, 2.0f);
+    }
+}
