bdcad09e7491ec3d7706122857e1c892cda1029d
Matt Joncas <mattjoncas@users.noreply.github.com>
2014-04-20 23:49:07 -0400

    Added pendulum trap.
    - pendulum trap works.
    - currently runs on wake up, needs to run of trigger.
    - needs to damage player.
    - needs to be added to the maze gen.

---
+++ working_project/Assets/Scripts/trap_stuff/PendulumTrap.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PendulumTrap : Trap {
+	
+	private float W = Mathf.Sqrt(9.8f / 1.5f);
+	private float D = Mathf.PI / 2;
+	private float MAX_ANGLE;// = transform.rotation.z * Mathf.PI / 180;
+	private float angle;
+	private float time;
+	
+	void Start() {
+		time = Time.time;
+		MAX_ANGLE = transform.rotation.z * Mathf.PI / 180;
+		activate();
+	}
+	
+	
+	
+	void Update() {
+		if (state == TrapState.BEGUN && !hasInvokedEnd) {
+			//hasInvokedEnd = true;
+			//end();
+
+		}
+		time += Time.deltaTime;
+		angle = MAX_ANGLE * Mathf.Sin(W * time + D);
+		angle *= 180 / Mathf.PI;
+		
+		Quaternion rot = transform.rotation;
+		rot.z = angle;
+		transform.rotation = rot;
+	}
+	
+	
+	public override void begin() {
+		if (state == TrapState.IDLE && isActivated) {
+			base.begin();
+			//transform.position += new Vector3(0, 1, 0);
+
+		}
+	}
+	
+	
+	
+	public override void end() {
+		if (state == TrapState.BEGUN) {
+			base.end();
+			
+		}
+	}
+}
